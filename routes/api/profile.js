2abc27ff0959d78bae4f4df3a14bcba9b476dc30
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-06-06 13:50:52 -0700

    Delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -142,8 +142,28 @@ router.get('/user/:user_id', async (req, res) => {
   } catch (err) {
     console.error(err.message);
     if (err.kind == 'ObjectId') {
-      res.status(400).json({ msg: 'Profile not found' });
+      return res.status(400).json({ msg: 'Profile not found' });
     }
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   DELETE api/profile
+// @desc    Delete profile, user & posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User removed' });
+  } catch (err) {
+    console.error(err.message);
     res.status(500).send('Server error');
   }
 });
