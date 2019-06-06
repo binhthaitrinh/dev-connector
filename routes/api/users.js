b9ab556c072f1144de8b693bdb6137985bb7b431
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-06-06 10:38:18 -0700

    User registration with post request to /api/users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register user
@@ -19,19 +22,69 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     // handle response
 
     const errors = validationResult(req);
 
-    // if errors
+    // if errors, stop here, that's why the return
 
     if (!errors.isEmpty()) {
       // send status, along with array of errors
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('User route');
+    // Destructure req.body
+
+    const { name, email, password } = req.body;
+
+    // See if user exists
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        // map error to array so that on fontend, we get same type of error, easier to handle
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // Get users gravatar
+
+      const avatar = gravatar.url(email, {
+        //Size
+        s: '200',
+        //rating
+        r: 'pg',
+        //default gravatar
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt password using brypt
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      // save user to db
+
+      await user.save();
+
+      // Return jsonwebtoken
+
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
