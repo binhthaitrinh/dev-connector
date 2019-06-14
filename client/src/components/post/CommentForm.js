503883c90bb6caf716bb84f9e406fef8b53e05aa
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-06-14 11:31:11 -0700

    Add comment, like function

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -1,42 +1,42 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { addPost } from '../../actions/post';
+import { addComment } from '../../actions/post';
 
-const PostForm = ({ addPost }) => {
+const CommentForm = ({ addComment, postId }) => {
   const [text, setText] = useState('');
   return (
-    <div class="post-form">
-      <div class="bg-primary p">
-        <h3>Say Something...</h3>
+    <div className="post-form">
+      <div className="bg-primary p">
+        <h3>Leave a comment</h3>
       </div>
       <form
         onSubmit={e => {
           e.preventDefault();
-          addPost({ text });
+          addComment(postId, { text });
           setText('');
         }}
-        class="form my-1">
+        className="form my-1">
         <textarea
           name="text"
           cols="30"
           rows="5"
-          placeholder="Create a post"
+          placeholder="Create a comment"
           required
           value={text}
           onChange={e => setText(e.target.value)}
         />
-        <input type="submit" class="btn btn-dark my-1" value="Submit" />
+        <input type="submit" className="btn btn-dark my-1" value="Submit" />
       </form>
     </div>
   );
 };
 
-PostForm.propTypes = {
-  addPost: PropTypes.func.isRequired
+CommentForm.propTypes = {
+  addComment: PropTypes.func.isRequired
 };
 
 export default connect(
   null,
-  { addPost }
-)(PostForm);
+  { addComment }
+)(CommentForm);
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -3,7 +3,10 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
-  ADD_POST
+  ADD_POST,
+  GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from '../actions/types';
 const initialState = {
   posts: [],
@@ -19,6 +22,12 @@ export default function(state = initialState, action) {
       return {
         ...state,
         posts: payload,
+        loading: false
+      };
+    case GET_POST:
+      return {
+        ...state,
+        post: payload,
         loading: false
       };
     case POST_ERROR:
@@ -44,8 +53,26 @@ export default function(state = initialState, action) {
     case ADD_POST:
       return {
         ...state,
-        posts: [...state.posts],
-        payload,
+        posts: [payload, ...state.posts],
+
+        loading: false
+      };
+
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: { ...state.post, comments: payload },
+        loading: false
+      };
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.post.comments.filter(
+            comment => comment._id !== payload
+          )
+        },
         loading: false
       };
     default:
